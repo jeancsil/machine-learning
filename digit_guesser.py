e43ea9602c272119f18e270a0ee138401ee0b02a
Jean Silva <jeancsil@gmail.com>
2017-07-09 15:32:46 +0200

    Make variables self descriptive and create a testing set.

--- digit_guesser.py
+++ digit_guesser.py
@@ -1,5 +1,3 @@
-import matplotlib.pyplot as plt
-
 from sklearn import datasets
 from sklearn import svm
 
@@ -8,15 +6,13 @@ digits = datasets.load_digits()
 clf = svm.SVC(gamma=0.0001, C=100)
 
 training_set = digits.data[:-10]
-labels = digits.target[:-10]
+training_labels = digits.target[:-10]
 
-x, y = training_set, labels
+testing_set = digits.data[-10:]
+testing_labels = digits.target[-10:]
+
+x, y = training_set, training_labels
 clf.fit(x, y)
 
 for i in range(10):
-    print("Prediction: {}".format(clf.predict([digits.data[-i]])))
-    print("Digit:      [{}]".format(digits.target[-i]))
-
-# print('Prediction: ',  clf.predict([digits.data[-1]]))
-# plt.imshow(digits.images[-1], cmap=plt.cm.gray_r, interpolation='nearest')
-# plt.show()
+    print("Test set: {}. Predicted: {}".format(testing_labels[i], clf.predict([testing_set[i]])[0]))
